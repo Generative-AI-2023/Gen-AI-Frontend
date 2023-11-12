6b4bf5d0b6af5683d1b43c954377c39897016d1c
JasperGrant <72110751+JasperGrant@users.noreply.github.com>
2023-11-12 13:39:04 -0400

    Merged Tori and I's things

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,18 +1,35 @@
+import { useEffect, useState } from "react";
 import ActivityList from "./Activities";
 import CheckboxList from "./Checkboxes";
 import DateRangePicker from "./DatePicker";
-// import CheckboxVerticalListGroup from "./CheckboxList";
 
-export default function Homepage(){
+export default function Homepage() {
+
+    const [itinerary, setItinerary] = useState([])
+
+    useEffect(() => {
+        itineraryget()
+    }, [])
+
+    const itineraryget = async () => {
+
+        const response = await fetch('https://holidai-backend-0c4977b424d6.herokuapp.com/i',
+            {
+                method: 'GET'
+            }
+        )
+        setItinerary(await response.text())
+
+    }
+
     return(
         <div>
-            Welcome to Holidai, Please fill out the following form for your itinerary.
             <CheckboxList />
+            {itinerary}
             {/* <CheckboxVerticalListGroup />     */}
             
             <ActivityList />
             <DateRangePicker/>
-
         </div>
         
     )
