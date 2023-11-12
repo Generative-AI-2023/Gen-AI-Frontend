aa019b7742f0fa187d60f84a28f8763d1df5c62e
JasperGrant <72110751+JasperGrant@users.noreply.github.com>
2023-11-12 15:32:28 -0400

    Added loading and nicer about

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,19 @@
+import Logo from "../img/logo.jpg"
+
 export default function About(){
     return(
-        <div>
-            About
+        <div className="text-center text-slate-900">
+            <br/>
+            <img src={Logo} className="mx-auto text-center h-64 w-100 rounded-lg" width="256" height="256" alt="Logo"/>
+            <br/>
+            <br/>
+            HolidAI is an app that uses generative AI to produce an itinerary based on the user's characteristics and preferences.
+            <br/>
+            <br/>
+            The client will input information pertaining to their budget, location, and age and the AI will handle the rest.
+            <br/>
+            <br/>
+            This app takes away the pain of planning so the end user can have more time enjoying their vacation
         </div>
     )
 }
---
+++ src/components/Loading.js
@@ -0,0 +1,7 @@
+import Cat from '../img/cat.gif'
+
+export default function About(){
+    return(
+        <img src={Cat} className="fill-current h-48 w-48 mr-2" width="256" height="256" alt="Logo"/>
+    )
+}
